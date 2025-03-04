c6c40d33ed8c7a836ad39788e74f57da7f3c6420
aaaasmirnova <smirnovnana@yandex.ru>
2025-03-04 23:31:06 +0300

    Добавляет сортировку пользователей по городу

---
+++ src/components/SortingByCity/SortingByCity.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { users } from "./data";
+import "./styles.css";
+export const SortingByCity = () => {
+  const [usersCity, setUsersCity] = useState([]);
+  const [activeButton, setActiveButton] = useState(null);
+
+  const getCities = () => {
+    return users
+      .map((user) => user.city)
+      .filter((city, index, arr) => arr.indexOf(city) === index);
+  };
+
+  const cities = getCities();
+
+  const getSortedByCityList = (city) => {
+    setUsersCity(users.filter((elem) => elem.city === city));
+    setActiveButton(city);
+  };
+
+  return (
+    <div className="users-wrapper">
+      <div className="city-buttons-wrapper">
+        {cities.map((city) => (
+          <button
+            className={`city-button ${
+              activeButton === city ? "city-button-active" : ""
+            }`}
+            onClick={() => getSortedByCityList(city)}
+          >
+            {city}
+          </button>
+        ))}
+      </div>
+      <div className="users-info-wrapper">
+        {usersCity.map((elem) => (
+          <div className={` users-info ${!elem.active ? "inactive-user" : ""}`}>
+            <p>
+              {elem.name}, возраст: {elem.age}, {elem.city}
+            </p>
+            <p>{elem.active ? "На сайте" : "Не на сайте"}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
---
+++ src/components/SortingByCity/data.js
@@ -0,0 +1,11 @@
+export const users = [
+  { id: "1", name: "Павел", age: "46", city: "Москва", active: true },
+  { id: "2", name: "Ольга", age: "23", city: "Саратов", active: false },
+  { id: "3", name: "Иван", age: "32", city: "Самара", active: true },
+  { id: "4", name: "Игорь", age: "54", city: "Самара", active: false },
+  { id: "5", name: "Вадим", age: "12", city: "Самара", active: true },
+  { id: "6", name: "Елена", age: "34", city: "Новосибирск", active: true },
+  { id: "7", name: "Алина", age: "26", city: "Москва", active: false },
+  { id: "8", name: "Дмитрий", age: "16", city: "Саратов", active: true },
+  { id: "9", name: "Марина", age: "34", city: "Новосибирск", active: false },
+];
